6e959ffd00bc2bcaf7f524f90d08186d6b959a99
parthvinchhi <parthvinchhi21112000@gmail.com>
2025-05-24 14:51:47 +0530

    Fresh start

--- run.bat
+++
@@ -1,2 +0,0 @@
-go build -o parth.exe cmd/web/main.go cmd/web/middleware.go cmd/web/routes.go
-parth.exe
--- run.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-go build -o bookings cmd/web/*.go
-./bookings -dbname=bread-n-breakfast -dbuser=postgres -dbpass=123 -cache=false -production=false
